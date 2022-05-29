edd34acee0d2cb4aa9e094bb2d3171e73747cf9f
“thaheer” <thaheer43@gmail.com>
2022-05-29 21:44:51 +0530

    added: Attendance-check

---
+++ src/com/employeewage/www/EmployeeWage.java
@@ -0,0 +1,19 @@
+package com.employeewage.www;
+ 
+import java.util.Random;
+
+public class EmployeeWage {
+
+	public static void main(String[] args) {
+		System.out.println(" Welcome to Employee Wage Computation Program ");
+		Random random = new Random();
+		int attendance = random.nextInt(2);
+		if (attendance == 1)
+
+			System.out.println("Employee is present");
+		else
+			System.out.println("Employee is absent");
+
+	}
+
+}
