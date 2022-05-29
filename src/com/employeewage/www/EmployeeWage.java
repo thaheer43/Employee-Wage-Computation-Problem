2ff387251fe216025fea0bf39cdf3dbd37b0111d
“thaheer” <thaheer43@gmail.com>
2022-05-29 21:49:18 +0530

    Employee wage program

--- src/com/employeewage/www/EmployeeWage.java
+++ src/com/employeewage/www/EmployeeWage.java
@@ -1,11 +1,12 @@
 package com.employeewage.www;
- 
+
 import java.util.Random;
 
 public class EmployeeWage {
 
 	public static void main(String[] args) {
 		System.out.println(" Welcome to Employee Wage Computation Program ");
+		
 		Random random = new Random();
 		int attendance = random.nextInt(2);
 		if (attendance == 1)
