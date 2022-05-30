fb2983807e2d8d508e71501223c3032abb995295
“thaheer” <thaheer43@gmail.com>
2022-05-30 15:59:30 +0530

    added: part time logic

--- src/com/employeewage/www/EmployeeWage.java
+++ src/com/employeewage/www/EmployeeWage.java
@@ -7,16 +7,23 @@ public class EmployeeWage {
 	public static void main(String[] args) {
 		System.out.println(" Welcome to Employee Wage Computation Program ");
 		Random random = new Random();
-		int attendance = random.nextInt(2);
+		int attendance = random.nextInt(3);
 		int wagePerHour = 20;
 		int fullDayHours = 8;
 		int wagePerDay = 0;
+		int partTimeHours = 4;
 
 		if (attendance == 1) {
 			System.out.println("Employee is present");
 			wagePerDay = fullDayHours * wagePerHour;
 			System.out.println("Employee's daily wage is:" + wagePerDay);
-		} else
+		} else if (attendance == 2) {
+			System.out.println("Employee is present for part time");
+			wagePerDay = partTimeHours * wagePerHour;
+			System.out.println("Employee's daily wage is:" + wagePerDay);
+		}
+
+		else
 			System.out.println("Employee is absent");
 
 	}
